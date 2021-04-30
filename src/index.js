111909c820f1181d6fde3300f91bd6171cdcea5f
serhiylevytskyy83 <serhiylevytskyy83@gmail.com>
2021-04-30 19:12:56 -0400

    Model

--- src/index.js
+++ src/index.js
@@ -13,28 +13,36 @@
 const express = require('express');
 // import getHome from './modules/home/home';
 // import postInfo from './modules/info/info';
-const errorHandler = require('./modules/core/errorHandler');
-const logger = require('./modules/core/logger');
-
-const parseResponse = require('./modules/core/parseResponse');
-const cors = require('./modules/core/cors');
-const routes = require('./modules/core/routes');
-const dbConnect = require('./modules/core/db');
+// const errorHandler = require('./modules/core/errorHandler');
+// const logger = require('./modules/core/logger');
+//
+// const parseResponse = require('./modules/core/parseResponse');
+// const cors = require('./modules/core/cors');
+// const routes = require('./modules/core/routes');
+// const dbConnect = require('./modules/core/db');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 // localhost 5000
 
-dbConnect();
-logger(app);
-parseResponse(app);
-cors(app);
-routes(app);
+// dbConnect();
+// logger(app);
+// parseResponse(app);
+// cors(app);
+// routes(app);
+//
+app.get('/', home);
+function home(req, res) {
+  res.json({
+    name: 'Joe',
+    lastname: ' Smith',
+    age: 20,
+  });
+}
 
-// app.get('/', getHome);
-// app.post('/info', postInfo);
-
-errorHandler(app);
+// // app.post('/info', postInfo);
+//
+// errorHandler(app);
 
 app.listen(PORT, () => {
   console.log(`Example app listening at http://localhost:${PORT}`);
