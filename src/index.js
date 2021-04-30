8c6a504573f9caa3e2bc70ce433dbbf2d1fc130c
serhiylevytskyy83 <serhiylevytskyy83@gmail.com>
2021-04-29 22:22:26 -0400

    Model

--- src/index.js
+++ src/index.js
@@ -9,31 +9,42 @@
 // // // const b = get(obj,'name','---')
 // // // console.log(b)
 
-import express from 'express';
+// import express from 'express';
 // import getHome from './modules/home/home';
 // import postInfo from './modules/info/info';
-import errorHandler from './modules/core/errorHandler';
-import logger from './modules/core/logger';
-import parseResponse from './modules/core/parseResponse';
-import cors from './modules/core/cors';
-import routes from './modules/core/routes';
-import dbConnect from './modules/core/db';
-
+// import errorHandler from './modules/core/errorHandler';
+// import logger from './modules/core/logger';
+// import parseResponse from './modules/core/parseResponse';
+// import cors from './modules/core/cors';
+// import routes from './modules/core/routes';
+// import dbConnect from './modules/core/db';
+const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
 // localhost 5000
 
-dbConnect();
-logger(app);
-parseResponse(app);
-cors(app);
-routes(app);
+// dbConnect();
+// logger(app);
+// parseResponse(app);
+// cors(app);
+// routes(app);
 
-// app.get('/', getHome);
+app.get('/', home);
+function home(req, res) {
+  res.json({
+    name: 'Joe',
+    lastname: ' Smith',
+    age: 20,
+  });
+}
 // app.post('/info', postInfo);
 
-errorHandler(app);
+// errorHandler(app);
 
 app.listen(PORT, () => {
   console.log(`Example app listening at http://localhost:${PORT}`);
 });
+
+//"start": "nodemon --inspect --exec babel-node src/index.js",
+//     "test": "echo \"Error: no test specified\" && exit 1",
+//     "babel-node": "babel-node --presets=@babel/preset-env --ignore='foo|bar|baz'"
