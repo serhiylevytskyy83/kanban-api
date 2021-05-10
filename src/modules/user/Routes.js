41c90cecf70d6c535eac3138c02ff1e805ec33a1
serhiylevytskyy83 <serhiylevytskyy83@gmail.com>
2021-05-10 17:35:37 -0400

    auth commit

--- src/modules/user/Routes.js
+++ src/modules/user/Routes.js
@@ -5,14 +5,14 @@ const userGetById = require('./userGetById');
 const userUpdateById = require('./userUpdateById');
 const userDeleteById = require('./userDeleteById');
 const userDeleteAll = require('./userDeleteAll');
-const authorization = require('../core/authorization');
+// const authorization = require('../core/authorization');
 
 const router = Router();
-router.post('/add', authorization, userRegister); // localhost5000/
-router.get('/', authorization, userGetAll); // localhost/user
-router.get('/:userId', userGetById); // localhost/user/id
-router.patch('/:userId', userUpdateById); // localhost/user/id
-router.delete('/:userId', userDeleteById); // localhost/user/id
-router.delete('/', userDeleteAll); // localhost/user
+router.post('/add', userRegister);
+router.get('/', userGetAll);
+router.get('/:userId', userGetById);
+router.patch('/:userId', userUpdateById);
+router.delete('/:userId', userDeleteById);
+router.delete('/', userDeleteAll);
 
 module.exports = router;
